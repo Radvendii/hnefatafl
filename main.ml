a47b7d584876343225ce9458dda59d9290851f1b
Liz <eav38@cornell.edu>
2015-11-08 14:28:30 -0500

    tried to establish possible move space, not completed

--- main.ml
+++ main.ml
@@ -31,6 +31,36 @@ let rec pop_find f = function
     then (xs, Some x)
     else let (xs', x') = pop_find f xs in (x::xs', x')
 
+type direction = Up | Down | Left | Right
+
+let closest_pieces (a,b) bd =
+  let contact_pieces_in_yPos = List.filter (fun (_,(x,y)) -> x=a && y>b) bd.pieces in
+  let contact_pieces_in_xPos = List.filter (fun (_,(x,y)) -> y=b && x>a) bd.pieces in
+  let contact_pieces_in_yNeg = List.filter (fun (_,(x,y)) -> x=a && y<b) bd.pieces in
+  let contact_pieces_in_xNeg = List.filter (fun (_,(x,y)) -> y=b && x<a) bd.pieces in
+  let sorted_in_xPos = List.sort (fun (_,(x,_)) (_,(x',_)) -> x-x') contact_pieces_in_xPos in
+  let sorted_in_yPos = List.sort (fun (_,(_,y)) (_,(_,y')) -> y-y') contact_pieces_in_yPos in
+  let sorted_in_xNeg = List.sort (fun (_,(x,_)) (_,(x',_)) -> x'-x) contact_pieces_in_xNeg in
+  let sorted_in_yNeg = List.sort (fun (_,(_,y)) (_,(_,y')) -> y'-y) contact_pieces_in_yNeg in
+  [(if List.len sorted_in_xPos > 0 then Some (List.hd sorted_in_xPos) else None);
+   (if List.len sorted_in_yPos > 0 then Some (List.hd sorted_in_yPos) else None);
+   (if List.len sorted_in_xNeg > 0 then Some (List.hd sorted_in_xNeg) else None);
+   (if List.len sorted_in_yNeg > 0 then Some (List.hd sorted_in_yNeg) else None);]
+
+(*let valid_move_space (p:piece) ((a,b):coord) (bd:board) : coord list =
+  let contact_pieces = closest_pieces (a,b) bd in
+  let rec helper (p:piece) ((x,y):coord) (bd:board) (d:direction) =
+    match (d,p) with
+    |(Right,WKing)-> let issue_piece = List.nth contact_pieces 0 in
+    |(Right,_)    -> let issue_piece = List.nth contact_pieces 0 in
+    |(Up, WKing)  -> let issue_piece = List.nth contact_pieces 1 in
+    |(Up,_)       -> let issue_piece = List.nth contact_pieces 1 in
+    |(Left,WKing) -> let issue_piece = List.nth contact_pieces 2 in
+    |(Left,_)     -> let issue_piece = List.nth contact_pieces 2 in
+    |(Down,WKing) -> let issue_piece = List.nth contact_pieces 3 in
+    |(Down,_)     -> let issue_piece = List.nth contact_pieces 3 in
+*)
+
 let () =
   init () ;
   loop_while (fun b ->
