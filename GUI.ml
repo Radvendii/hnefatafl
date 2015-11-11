941b74be6e461955442cc87861e47fba6f2dbc9c
Radvendii <taeer@necsi.edu>
2015-11-11 01:55:20 +0000

    added for-later function to GUI spec

--- GUI.ml
+++ GUI.ml
@@ -1,8 +1,9 @@
 open Game_types
 
 module type GUI = sig
-  val init : unit -> unit
-  val deinit : unit -> unit
+  val init       : unit -> unit
+  val deinit     : unit -> unit
   val draw_board : board -> unit
   val user_input : unit -> action
+  (* val menu       : (string * 'a) list -> 'a *)
 end
