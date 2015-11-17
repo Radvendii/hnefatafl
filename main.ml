2690732de814e711ff3b1915fef0152cdb833a2e
Radvendii <taeer@necsi.edu>
2015-11-17 18:51:15 -0500

    crashes on win. :D

--- main.ml
+++ main.ml
@@ -1,7 +1,7 @@
 open Helpers
 open Game_types
-open Graphical.GraphicsGUI
-(* open Ascii.AsciiGUI *)
+(* open Graphical.GraphicsGUI *)
+open Ascii.AsciiGUI
 open Menu
 
 (* TODO? read in inital positions and rules from textfile *)
@@ -154,14 +154,18 @@ let valid_move c1 c2 b =
 
 let () =
   init () ;
-  (* let _ = initmenu () in *)
+  let _ = initmenu () in
   loop_while (fun b ->
       draw_board b ;
-      match user_input () with
-      | Move(c1, c2) ->
-           if not @@ valid_move c1 c2 b then Cont(b)
-           else Cont
-             { b with
+      match player_won b with
+      | Some(White) -> Break(())
+      | Some(Black) -> Break(())
+      | None ->
+        match user_input () with
+        | Move(c1, c2) ->
+          if not @@ valid_move c1 c2 b then Cont(b)
+          else Cont
+              { b with
                 turn = next_turn b.turn ;
                 pieces =
                   let ps, p1 = pop_find (fun (_,c) -> c = c1) b.pieces in
@@ -169,11 +173,11 @@ let () =
                   match p1 with
                   | None -> failwith "checked for in valid_move"
                   | Some(p1') ->
-                      let nps = (fst p1', c2)::ps' in (* move the piece!*)
-                      let rps = piece_taken c2 {b with pieces = nps} in
-                      List.filter (fun x -> not @@ List.mem (snd x) rps) nps
+                    let nps = (fst p1', c2)::ps' in (* move the piece!*)
+                    let rps = piece_taken c2 {b with pieces = nps} in
+                    List.filter (fun x -> not @@ List.mem (snd x) rps) nps
               }
-      | Quit -> Break(())
-      | Nop -> Cont(b)
+        | Quit -> Break(())
+        | Nop -> Cont(b)
     ) init_board ;
   deinit ()
