1407b38ecfc51378a9693adc3717fd92f89f6fff
Radvendii <taeer@necsi.edu>
2015-12-01 19:16:39 -0500

    moved copenhagen and updated module lists

--- GUI_list.ml
+++ GUI_list.ml
@@ -44,4 +44,7 @@ let default_gui = "graphical", (module Graphical.GUI : GUI)
 let gui_list =
   [ "ascii", (module Ascii.GUI : GUI)
   ; "graphical", (module Graphical.GUI : GUI)
+  ; "openGL", (module OpenGL.GUI : GUI)
+  ; "sdl", (module Sdl.GUI : GUI)
+  ; "threeD", (module ThreeD.GUI : GUI)
   ]
